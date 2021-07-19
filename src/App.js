8742f6878dd20432cb7c1f8f8163dff00f4530d0
Karen Pacheco <karensilva120@hotmail.com>
2021-07-19 02:42:07 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import './App.css';
+import image1 from './image-template.png'
+ 
+
+function msgConfirmacao()
+{
+  alert("Email enviado!");
+}
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <center><h1 id="sobre">Promocoes imperdiveis para voce! <br/> De uma olhada no cardapio, sao diversos sabores e ofertas! <br/></h1></center>
+      <img src={image1}/>
+      <br/>
+      <br/>
+      <form>
+        <label htmlFor="email">E-mail</label>
+        <input type="text" id="email" name="email" placeholder="E-mail de destino.." />
+        <input type="submit" onclick='msgConfirmacao()' value="Enviar" />
+      </form>
     </div>
   );
 }
+
+
 
 export default App;
