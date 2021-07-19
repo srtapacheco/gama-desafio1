972ee4295bb32e9ca44792618264802952484419
Karen Pacheco <64283812+srtapacheco@users.noreply.github.com>
2021-07-19 02:23:03 -0300

    Update1 App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <div className="container">
+            <center><h1 id="sobre">Promocoes imperdiveis para voce! <br/> De uma olhada no cardapio, sao diversos sabores e ofertas! <br/></h1></center>
+            <img src={imagePrinc }/>
+            <br/>
+            <br/>
+            <form>
+              <label htmlFor="email">E-mail</label>
+              <input type="text" id="email" name="email" placeholder="E-mail de destino.." />
+              <input type="submit" onclick='msgConfirmacao()' value="Enviar" />
+            </form>
+          </div>
         </p>
         <a
           className="App-link"
